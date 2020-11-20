9ad60c5e730d03529d6842cdbfea42e93e18c70f
Ahmet Levent Tasel <leventtasel@gmail.com>
2020-11-19 23:55:15 -0800

    TaskAlgo : build a simple graph for unit testing
    should probably cover more cases but it will do for now

--- python/DemoGafferExtensionTest/TaskAlgoTest.py
+++ python/DemoGafferExtensionTest/TaskAlgoTest.py
@@ -1,6 +1,8 @@
 import unittest
 
+import Gaffer
 import GafferTest
+import GafferScene
 
 from DemoGafferExtension import TaskAlgo
 
@@ -11,6 +13,22 @@ class TaskAlgoTest( GafferTest.TestCase ) :
 
 		# check if the function gracefully returns empty list
 		self.assertEqual( TaskAlgo.getTaskFileNames( None ), [] )
+
+		scriptNode = Gaffer.ScriptNode()
+		scriptNode["reader"] = GafferScene.SceneReader()
+		scriptNode["reader"]["fileName"].setValue( "/some/scene/path" )
+		scriptNode["camera"] = GafferScene.Camera()
+		scriptNode["group"] = GafferScene.Group()
+		scriptNode["group"]["in"][-1].setInput( scriptNode["reader"]["out"] )
+		scriptNode["group"]["in"][-1].setInput( scriptNode["camera"]["out"] )
+		scriptNode["render"] = GafferScene.OpenGLRender()
+		scriptNode["render"]["fileName"].setValue( "/some/output/path" )
+		scriptNode["render"]["in"].setInput( scriptNode["group"]["out"] )
+
+		self.assertEqual(
+			TaskAlgo.getTaskFileNames( scriptNode["render"]["task"] ),
+			['/some/scene/path', '/some/output/path']
+		)
 
 
 if __name__ == "__main__":
